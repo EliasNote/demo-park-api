80b43ee1581723403262e2723232d327810c6be6
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-04-25 09:19:02 -0300

    Métodos de testes foram implementados

--- src/main/java/com/mballen/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/mballen/demoparkapi/web/controller/UsuarioController.java
@@ -79,8 +79,9 @@ public class UsuarioController {
                     @ApiResponse(responseCode = "400", description = "Senha não confere",
                             content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
                     @ApiResponse(responseCode = "404", description = "Recurso não encontrado",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))
-                    )
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Campos inválidos ou mal formatados",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
             })
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updatePassword(@PathVariable Long id,@Valid @RequestBody UsuarioSenhaDto dto) {
