44d02946629679c3fc9693a0129e19a47bf126bd
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-04-26 08:16:12 -0300

    Sisitema de autenticação implementada

--- src/main/java/com/mballen/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/mballen/demoparkapi/service/UsuarioService.java
@@ -6,6 +6,7 @@ import com.mballen.demoparkapi.exception.PasswordInvalidException;
 import com.mballen.demoparkapi.exception.UsernameUniqueViolationException;
 import com.mballen.demoparkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,9 +16,12 @@ import java.util.List;
 @Service
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Transactional
     public Usuario salvar(Usuario usuario) {
         try {
+            usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
             return usuarioRepository.save(usuario);
         } catch (org.springframework.dao.DataIntegrityViolationException ex) {
             throw new UsernameUniqueViolationException(String.format("Username {%s} já cadastrado", usuario.getUsername()));
@@ -42,12 +46,22 @@ public class UsuarioService {
         if (!novaSenha.equals(confirmaSenha)) {
             throw new PasswordInvalidException("Nova senha não confere com confirmação de senha.");
         }
-        if (!senhaAtual.equals(user.getPassword())) {
+        if (!passwordEncoder.matches(senhaAtual, user.getPassword())) {
             throw new PasswordInvalidException("Sua senha não confere.");
         }
-        user.setPassword(novaSenha);
+        user.setPassword(passwordEncoder.encode(novaSenha));
         return user;
     }
 
+    @Transactional(readOnly = true)
+    public Usuario buscarPorUsername(String username) {
+        return usuarioRepository.findByUsername(username).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Usuário com username=%s não encontrado", username))
+        );
+    }
 
+    @Transactional(readOnly = true)
+    public Usuario.Role buscarRolePorUsername(String username) {
+        return usuarioRepository.findRoleByUsername(username);
+    }
 }
